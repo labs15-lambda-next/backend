bc2f85096e2df8aa448cdc7c97bfbd07a122b559
Aquariax <roberthrrs8@gmail.com>
2019-08-22 18:19:47 -0400

    two new models

--- models/users-model.js
+++ models/users-model.js
@@ -1,10 +1,31 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  add,
+  getUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  updateUser
 };
 
 
-function add(user) {
-  return db('users').insert(user, 'id');
+function getUsers() {
+  return db('users').select('id', 'full_name', 'email', 'problem_id');
+}
+function getUserById(id) {
+  return db('users').where({ id }).first();
+}
+async function addUser(user) {
+  const [id] = await db('users').insert(user);
+  return getUserById(id);
+}
+function updateUser(id, changes) {
+  return db('users')
+    .where({ id })
+    .update(changes);
+}
+function deleteUser(id) {
+  return db('users')
+    .where('id', id)
+    .del();
 }
