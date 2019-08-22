f12e2ed684bcdc6cc832a20be91dd048b7a44b3a
alanperez <alanperez9six@gmail.com>
2019-08-22 14:13:32 -0500

    user router post

--- controllers/users-routes.js
+++ controllers/users-routes.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 // Model
+const Users = require('../data/dbConfig');
 
-// router.get('/', async(req,res) => {
-//   try {
-//     const
-//   }
-// })
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users('users');
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  const { full_name, email } = req.body;
+
+  if (!full_name || !email) {
+    res.status(404).json({ message: 'Enter your name and email' });
+  } else {
+    Users.insert(req.body)
+      .then((user) => {
+        res.status(200).json({
+          message: `new user signed up: ${user}`,
+          user: req.body
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: `error 500 hundo, ${error}` });
+      });
+  }
+});
 
 module.exports = router;
