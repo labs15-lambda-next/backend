5db95c0d0aee5ac6de780fd0784467b157d37ba0
alanperez <alanperez9six@gmail.com>
2019-08-23 11:09:08 -0500

    changed up addUser function

--- models/users-model.js
+++ models/users-model.js
@@ -16,8 +16,10 @@ function getUserById(id) {
   return db('users').where({ id }).first();
 }
 async function addUser(user) {
-  const [id] = await db('users').insert(user);
-  return getUserById(id);
+  const response = await db('users').insert(user);
+  return response;
+  // const [id] = await db('users').insert(user);
+  // return getUserById(id);
 }
 function updateUser(id, changes) {
   return db('users')
