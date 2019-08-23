2c945a158adff9af4847aca321bac05d96cd2655
alanperez <alanperez9six@gmail.com>
2019-08-23 16:57:25 -0500

    :sparkles: sendgrid
    sending emails fer dayssss

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -1,11 +1,11 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SEND_GRID_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
+  to: 'Labs15teamnext@gmail.com',
+  from: 'alanperez9six@gmail.com',
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  html: '<strong>and easy to do anywhere, even with Node.js </strong>',
 };
 sgMail.send(msg);
