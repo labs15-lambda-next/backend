494a851a7554181b7645b30ab1c6c117356122e9
Agustina Sosa <agustinasf85@gmail.com>
2019-08-23 16:30:40 -0400

    :sparkles: Setting up Sendgrid
    Setting up Sendgrip, added dependency.

--- data/seeds/02-problems.js
+++ data/seeds/02-problems.js
@@ -3,6 +3,7 @@ exports.seed = function (knex) {
   return knex('problems').del()
     .then(() =>
     // Inserts seed entries
+      // eslint-disable-next-line implicit-arrow-linebreak
       knex('problems').insert([
         {
           id: 1, admin_id: 1, problem_title: 'Ieed to keep track of my medical stats', problem_description: 'Medical stats.....', problem_category: 'Health', date_created: '2019', created_by: 'Joe'
