49d5a4f893d7895b1d71d5e5bd7ebc29acd6af38
Alan Perez <alanp9six@gmail.com>
2019-08-29 15:23:07 -0500

    ✨ Emails Admin
    BUG: Sends a single email for each user.

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -12,9 +12,9 @@ const sendEmails = async () => {
   emails.forEach(user => {
     const msg = {
       to: 'labs15teamnext@gmail.com',
-      from: 'test@example.com',
+      from: 'noreply@labs15teamnext.com',
       subject: 'Sending with Twilio SendGrid is Fun',
-      html: `<strong>and easy to do anywhere, even with Node.js ${user.email}</strong>`,
+      html:`<strong>List of users:</strong><br>${user.full_name}<br>${user.email}`,
     };
 
     sgMail.send(msg);
