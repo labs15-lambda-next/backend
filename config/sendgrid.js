691c0a5310c1489cdfa58561f93c4340bdecb9de
Alan Perez <alanp9six@gmail.com>
2019-08-29 17:59:01 -0500

    :bug: commented the sendgrid. dont want to fill up our mailbox

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -1,24 +1,24 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
-const userEmails = require('../models/users-model');
+// const userEmails = require('../models/users-model');
 
-sgMail.setApiKey('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');
+// sgMail.setApiKey('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');
 
-const sendEmails = async () => {
-  const emails = await userEmails.getUserEmail('users');
+// const sendEmails = async () => {
+//   const emails = await userEmails.getUserEmail('users');
 
-  emails.map(user => {
-    const msg = {
-      to: 'labs15teamnext@gmail.com',
-      from: 'noreply@labs15teamnext.com',
-      subject: 'Sending with Twilio SendGrid is Fun',
-      html:`<strong>List of users:</strong><br>${user.full_name}<br>${user.email}`,
-    };
+//   emails.map(user => {
+//     const msg = {
+//       to: 'labs15teamnext@gmail.com',
+//       from: 'noreply@labs15teamnext.com',
+//       subject: 'Sending with Twilio SendGrid is Fun',
+//       html:`<strong>List of users:</strong><br>${user.full_name}<br>${user.email}`,
+//     };
 
-    sgMail.send(msg);
-  })
-}
+//     sgMail.send(msg);
+//   })
+// }
 
-sendEmails()
+// sendEmails()
