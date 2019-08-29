5e51e08437250561616dd1ee11ec5ddafcaa8b83
Alan Perez <alanp9six@gmail.com>
2019-08-29 15:14:55 -0500

    ✨ Shit Works
    wooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -4,20 +4,21 @@ const sgMail = require('@sendgrid/mail');
 
 const userEmails = require('../models/users-model');
 
-const emails = userEmails.getUserEmail('users');
-const to_list = [];
-
-// console.log(email);
-// for (i = 0; i > emails.length; i++) {
-//   to_list.push(emails[k]);
-// }
-console.log('list: ', emails);
 sgMail.setApiKey('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');
-const msg = {
-  to: 'labs15teamnext@gmail.com',
-  from: 'test@example.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: `${emails}`,
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+
+const sendEmails = async () => {
+  const emails = await userEmails.getUserEmail('users');
+
+  emails.forEach(user => {
+    const msg = {
+      to: 'labs15teamnext@gmail.com',
+      from: 'test@example.com',
+      subject: 'Sending with Twilio SendGrid is Fun',
+      html: `<strong>and easy to do anywhere, even with Node.js ${user.email}</strong>`,
+    };
+
+    sgMail.send(msg);
+  })
+}
+
+sendEmails()
