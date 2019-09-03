4076250f1b5f889235c699fb306ee464b78df6a4
Alan Perez <alanp9six@gmail.com>
2019-09-03 15:45:31 -0500

    fixing errors

--- controllers/Authentication/Authentication.js
+++ controllers/Authentication/Authentication.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const passport = require('passport')
-const GoogleStratgey = require('passport-google-oauth20').GoogleStrategy
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const router = express();
 
-passport.use(new GoogleStratgey({
-    clientID: process.env.GOOGLESECRET,
-    clientSecret: process.env.GOOGLEID,
-    callbackURL:'/auth/google/callback'
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken)
-    console.log(profile)
-    console.log(done)
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+  console.log(done);
 
-}))
+}));
 
 router.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
+  scope: ['profile', 'email']
+}));
 
-router.get('/auth/google/callback', passport.authenticate('google'))
+router.get('/auth/google/callback', passport.authenticate('google'));
+
+
+module.exports = router;
--- controllers/Authentication/auth-router.js
+++ controllers/Authentication/auth-router.js
@@ -1,26 +1,24 @@
-const router = require('express').Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const Users = require('../models/user-models');
+// const router = require('express').Router();
+// const jwt = require('jsonwebtoken');
+// const passport = require('passport');
 
-// Login with google
-router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-    prompt: 'select_account',
-  })
-);
+// // Login with google
+// router.get(
+//   '/google',
+//   passport.authenticate('google', {
+//     scope: ['profile', 'email'],
+//     prompt: 'select_account',
+//   })
+// );
 
-// google login redirect
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  res
-    .status(200)
-    .cookie('token', res.req.authInfo)
-    .redirect(`${process.env.FRONTEND_URL}/profile`);
-});
+// // google login redirect
+// router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+//   res
+//     .status(200)
+//     .cookie('token', res.req.authInfo)
+//     .redirect(`${process.env.FRONTEND_URL}/profile`);
+// });
 
 
 
-module.exports = router;
+// module.exports = router;
