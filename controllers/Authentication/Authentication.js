809abb3b75d59068fe3be9466e0a926fa33e3aad
Alan Perez <alanp9six@gmail.com>
2019-09-04 12:31:11 -0500

    commented previous code

--- controllers/Authentication/Authentication.js
+++ controllers/Authentication/Authentication.js
@@ -1,26 +1,49 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const router = require('express').Router();
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+// passport.use(new GoogleStrategy({
+//   callbackURL: '/auth/google/redirect',
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET
+// }, (accessToken, refreshToken, profile, done) => {
+//   console.log(accessToken);
+//   console.log(refreshToken);
+//   console.log(profile);
+//   console.log(done);
+//   console.log('dsadas');
+
+// }));
+
+// router.get('/google', passport.authenticate('google', {
+//   scope: ['profile', 'email']
+// }));
+
+// router.get('/auth/google/redirect', passport.authenticate('google'));
+
+
+// module.exports = router;
 
 const router = require('express').Router();
+const passport = require('passport');
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
-  console.log(done);
+// Login with google
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    prompt: 'select_account',
+  })
+);
 
-}));
-
-router.get('/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-router.get('/auth/google/callback', passport.authenticate('google'));
+// google login redirect
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  res
+    .status(200)
+    .cookie('token', res.req.authInfo)
+    .redirect(`${process.env.FRONTEND_URL}/home`);
+});
 
 
 module.exports = router;
