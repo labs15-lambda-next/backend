ea514fe050adc10585704cca941310f54b8b7eda
Raidon <Raidonmgmt@gmail.com>
2019-09-06 16:07:29 -0400

    Authorization Middleware

---
+++ controllers/Authentication/Authorization.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+function checkAuthentication(req, res, next) {
+    if(req.isAuthenticated()) {
+        next();
+    } else {
+       return res.redirect('/')
+    }
+}
+
+function checkAuthorization(req, res, next) {
+    if(req.user && req.user.role == 'admin') {
+        return next();
+    } else {
+        return res.status(500).json('You are not an administrator');
+    }
+}
+router.get('/google', checkAuthentication, checkAuthorization, function(req, res) {
+
+});
+
