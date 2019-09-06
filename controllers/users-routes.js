be002a7f58916e05603b9547a67d41f3d1884702
Alan Perez <alanp9six@gmail.com>
2019-09-06 11:38:11 -0500

    pushing what i have.. was testing the staging endpoint this whole time...

--- controllers/users-routes.js
+++ controllers/users-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 // Model
 const Users = require('../models/users-model');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 // sned msg
 // const sendMessage = require('../config/sendgrid');
@@ -19,16 +21,32 @@ router.get('/', async (req, res) => {
  *
  */
 router.post('/signup', async (req, res) => {
+  
   const { problem_id, full_name, email } = req.body;
 
   if (!problem_id, !full_name || !email) {
     res.status(404).json({ message: 'Enter your name and email' });
   } else {
+    
     Users.addUser(req.body)
       .then((user) => {
-        res.status(200).json({
+        const sendEmails = async () => {
+
+    const emails = await Users.getUserEmail('users')
+      emails.map(user => {
+    const msg = {
+      to: req.body.email,
+      from: 'noreply@labs15teamnext.com',
+      subject: 'Sending with Twilio SendGrid is Fun',
+      html:`<strong>List of users:</strong><br>${user.full_name}<br>${user.email}`,
+    };
+
+    // sgMail.send(msg);
+  })
+  }
+        res.status(200).json(sgMail.send(msg),{
           message: `new user signed up: ${email}`,
-          user: req.body
+          user: req.body,
         });
       })
       .catch((error) => {
