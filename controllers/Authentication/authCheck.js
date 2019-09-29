e132f32ba2086abad729061603b056ae90fa948c
alanperez <alanperez9six@gmail.com>
2019-09-28 20:05:00 -0500

    auth check for user

--- controllers/Authentication/authCheck.js
+++ controllers/Authentication/authCheck.js
@@ -1,8 +1,10 @@
 module.exports = authCheck;
 function authCheck(req, res, next) {
-  // console.log('req user:', req.user);
+  console.log('req user:', req.user);
+  console.log('req admin:', req.admin);
+  console.log('req.isAuthenticated', req.isAuthenticated());
   // console.log('req headers cookie: ', req.headers);
-  if (!req.headers.cookie) {
+  if (!req.user) {
     res.status(401).json({ message: 'Invalid Credentials. Please Log In' });
   } else {
     next();
