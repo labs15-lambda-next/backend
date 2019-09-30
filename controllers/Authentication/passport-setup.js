5840852cc49970cc02a5e0c934b8f5a0b8748a0e
alanperez <alanperez9six@gmail.com>
2019-09-30 16:16:59 -0500

    :sparkles: Admin Rights
    Assigns emails with @lambdaschool.com admin and also the team gmail

--- controllers/Authentication/passport-setup.js
+++ controllers/Authentication/passport-setup.js
@@ -35,7 +35,30 @@ const verifyGoogleUser = async (obj, done) => {
   // } catch (err) {
   //   console.log(err);
   // }
-  const user = await Admins.getByEmail(profile.emails[0].value).catch((err) => console.error(err));
+  try {
+    const user = await Admins.getByEmail(profile.emails[0].value);
+    // console.log('CLG FN USER.EMAIL', user.email);
+    // determine if they are a admin
+    if (profile.emails[0].value.includes('@lambdaschool.com') || profile.emails[0].value.includes('labs15teamnext@gmail.com')) {
+      // they are a admin
+      console.log('add admin');
+      console.log('CLG THIS THE USER', user);
+      if (!user) {
+        const [id] = await Admins.add({
+          email: profile.emails[0].value,
+          google_id: profile.id,
+        });
+        done(null, await Admins.getAdminById(id), token);
+      } else {
+        done(null, await Admins.getAdminById(user.id), token);
+      }
+    } else {
+      done('You do not have the rights', null, null);
+    }
+  } catch (err) {
+    console.log('CLG THE ERROR CATCH', err);
+    done(err, null, token);
+  }
   // const clientToken = generateToken(user);
   // console.log('clientToken', clientToken);
   // console.log('user.email ps', user.email);
@@ -47,18 +70,28 @@ const verifyGoogleUser = async (obj, done) => {
   // if (!profile._json.domain || profile.emails[0].value !== 'labs15teamnext@gmail.com') {
   //   done(null, false, { message: 'Not allow access!' });
   // }
-  try {
-    if (!user) {
-      // console.log('CLG FN USER.EMAIL', user.email);
-      const [id] = await Admins.add({
-        email: profile.emails[0].value,
-        google_id: profile.id,
-      });
-      done(null, await Admins.getAdminById(id), token);
-    } else {
-      done(null, user, token);
-    }
-  } catch (err) {
-    console.error(err);
-  }
+  // try {
+  //   if (!user) {
+  //     // console.log('CLG FN USER.EMAIL', user.email);
+  //     // determine if they are a admin
+  //     if (profile.emails[0].value.includes('@lambdaschool.com') || profile.emails[0].value.includes('labs15teamnext@gmail.com')) {
+  //       // they are a admin
+  //       console.log('add admin');
+  //       const [id] = await Admins.add({
+  //         email: profile.emails[0].value,
+  //         google_id: profile.id,
+  //       });
+  //       done(null, await Admins.getAdminById(id), token);
+  //     } else {
+  //       console.log('not allowed access');
+  //       // they are not a admin
+  //       done(null, false, { message: 'Not allow access!' });
+  //     }
+  //   } else {
+  //     done(new Error('Not allowed in here'), user, token);
+  //   }
+  // } catch (err) {
+  //   done(err, false, { message: 'Not allow access!' });
+  //   console.error(err);
+  // }
 };
